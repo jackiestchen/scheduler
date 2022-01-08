4a27a374828a9b3b3ee66e15370759751a2c1e7d
jackiestchen <jackiestchen@gmail.com>
2022-01-08 12:10:54 -0700

    Initialize Empty component

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Empty(props) {
+  return (
+    <main className="appointment__add">
+      <img
+        className="apppointment__add-button"
+        src="images/add.png"
+        alt="Add"
+      />
+    </main>
+  );
+}
