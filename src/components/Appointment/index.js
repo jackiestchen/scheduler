3329f65d38be9e13f124897ebe562617796df117
jackiestchen <jackiestchen@gmail.com>
2022-01-08 23:32:32 -0700

    Add components to Appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import "components/Appointment/styles.scss";
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 
 export default function Appointment(props) {
   return (
     <article className='appointment'>
-
+      <Header time={props.time} />
+      {props.interview ? <Show /> : <Empty />}
     </article>
   )
 }
