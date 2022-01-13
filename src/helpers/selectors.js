e5a7402e9182612d34ddd740192bdc04951601e8
jackiestchen <jackiestchen@gmail.com>
2022-01-12 19:37:04 -0700

    Add commnets

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,11 @@
+
+/** 
+ * Appointments selector function
+ * @param {object} state - an object contains appoinments and days
+ * @param {string} name - day 
+ * @returns an array of appointments for specific day
+ */
+
 const getAppointmentsForDay = (state, name) => {
 
   if (state.days.length < 1) {
