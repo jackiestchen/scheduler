31f927f6d721adcf96f59b7bfdf18e7d824b7eb9
jackiestchen <jackiestchen@gmail.com>
2022-01-12 20:18:39 -0700

    Add getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,13 +1,11 @@
-
-/** 
+/**
  * Appointments selector function
- * @param {object} state - an object contains appoinments and days
- * @param {string} name - day 
- * @returns an array of appointments for specific day
+ * @param {object} state - a React state object contains appointments and days
+ * @param {string} name - day
+ * @returns an array of appointments for specific day, returns null if state empty or name not found
  */
 
 const getAppointmentsForDay = (state, name) => {
-
   if (state.days.length < 1) {
     return [];
   }
@@ -18,8 +16,24 @@ const getAppointmentsForDay = (state, name) => {
     return [];
   }
 
-  return appointmentDay[0].appointments.map(id => state.appointments[id]);
+  return appointmentDay[0].appointments.map((id) => state.appointments[id]);
+};
 
-}
+/**
+ * Interviewer selector function
+ * @param {interviewers} state -a React state object contains interviewers object
+ * @param {student, interviewer} interview - interview object
+ * @returns interview object {student: name, interviewer: {id, name, avatar} }
+ */
 
-module.exports = {getAppointmentsForDay};
+const getInterview = (state, interview) => {
+  if (interview === null) {
+    return null;
+  }
+  interview.interviewer = state.interviewers[interview.interviewer];
+  return interview;
+};
+
+module.exports = { getAppointmentsForDay, getInterview };
+
+
