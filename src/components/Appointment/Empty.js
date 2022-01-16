84e274256c5d58e0faef9677641483baaca58ec8
jackiestchen <jackiestchen@gmail.com>
2022-01-16 00:55:15 -0700

    Fix typo in className

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Empty(props) {
   return (
-    <main className="appointment__add" >
+    <main className="appointment__add">
       <img
-        className="apppointment__add-button"
+        className="appointment__add-button"
         src="images/add.png"
         alt="Add"
         onClick={props.onAdd}
