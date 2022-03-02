04f8c76077bb46e06164368cc23955a2c2509e82
jackiestchen <jackiestchen@gmail.com>
2022-03-01 17:29:12 -0700

    Fix typo so tests pass

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,6 +2,8 @@ import React from "react";
 import classNames from "classnames";
 import "components/DayListItem.scss";
 
+//DayListItem component will display the remaining spots for a specific day. It will render in active if there are no remaining spots.
+
 export default function DayListItem(props) {
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
@@ -13,7 +15,7 @@ export default function DayListItem(props) {
       return `no spots remaining`;
     }
     if (spot === 1) {
-      return `${spot} spot remaiming`;
+      return `${spot} spot remaining`;
     }
     if (spot > 1) {
       return `${spot} spots remaining`;
