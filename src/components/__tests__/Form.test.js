b532ef2b104fde6b4e1a1bb7d413319ee5c6e7f6
jackiestchen <jackiestchen@gmail.com>
2022-03-12 23:08:47 -0700

    Add tests

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { cleanup, getByPlaceholderText, render } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
 import Form from "components/Appointment/Form";
 
 afterEach(cleanup);
@@ -13,11 +13,45 @@ describe("Form", () => {
     },
   ];
 
+  it("renders without crashing", () => {
+    render(<Form interviewers={interviewers} />);
+  });
+
   it("renders without student name if not provided", () => {
+    const { getByPlaceholderText } = render(
+      <Form interviewers={interviewers} />
+    );
     expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
   });
 
   it("renders with initial student name", () => {
+    const { getByTestId } = render(
+      <Form interviewers={interviewers} student="Lydia Miller-Jones" />
+    );
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
+
+  it("validates that the student name is not blank", () => {
+    const { getByText, onSave } = render(<Form interviewers={interviewers} />);
+
+    expect(getByText(/student name cannot be blank/i)).toBeNull();
+
+    expect(onSave).not.toHaveBeenCalled();
+  });
+
+  it("calls onSave function when the name is defined", () => {
+    /* 3. validation is not shown */
+    const { queryByText, onSave } = render(
+      <Form interviewers={interviewers} />
+    );
+
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+
+    /* 4. onSave is called once*/
+    expect(onSave).toHaveBeenCalledTimes(1);
+
+    /* 5. onSave is called with the correct arguments */
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+  });
+  
 });
