c97960fcbd8aafc61decdd389b5f4caf798e2eb8
jackiestchen <jackiestchen@gmail.com>
2022-03-21 23:36:05 -0600

    Add test for should navigate to Tuesday

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,5 +5,10 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
+
+    cy.get("li")
+      .contains("li", "Tuesday")
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
   });
 });
